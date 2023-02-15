34ea1d64d47748d1ae057d97bcb9911adc86239e
LeandroParisi <lepcbelisario@gmail.com>
2023-02-15 13:17:54 -0300

    database installation

---
+++ LinuxSetup/SystemSetup/scripts/7.InstallDatabaseGUIs.sh
@@ -0,0 +1,9 @@
+sudo snap install dbeaver-ce
+
+sudo snap install another-redis-desktop-manager
+
+# MONGO
+# https://studio3t.com/download/?utm_source=adwords&utm_medium=ppc&utm_term=studio%203t&utm_campaign=Leads-Search-244&hsa_net=adwords&hsa_ad=620123800280&hsa_src=g&hsa_ver=3&hsa_grp=144470069201&hsa_acc=1756351187&hsa_tgt=aud-401416985180:kwd-401232953436&hsa_mt=p&hsa_kw=studio%203t&hsa_cam=18249781362&gclid=Cj0KCQiAorKfBhC0ARIsAHDzslt27eJHx8XV80gczxrIuko1boegr2PqzeuTzPmuQrSK6fSnizegOqAaAvEdEALw_wcB
+# https://studio3t.com/knowledge-base/articles/how-to-install-studio-3t-on-linux/
+
+sudo snap install postman
