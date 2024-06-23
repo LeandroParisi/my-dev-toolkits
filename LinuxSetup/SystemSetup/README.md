893086d93bf79cad9172636870a647148fbd942c
LeandroParisi <lepcbelisario@gmail.com>
2024-06-23 20:40:20 +0200

    lts system setup backup

---
+++ LinuxSetup/SystemSetup/README.md
@@ -0,0 +1,4 @@
+1. Run all scripts inside scripts folder
+2. install these programs:
+   1. Ultimate Media Downloader
+   2. 
--- LinuxSetup/SystemSetup/user_backup/CreateBackup.sh
+++ LinuxSetup/SystemSetup/user_backup/CreateBackup.sh
@@ -1,7 +1,11 @@
 bashFilePath=$HOME/.zshrc
-outputPath=./my_scripts/my_custom_scripts.sh
+outputPathCustomScripts=./my_scripts/my_custom_scripts.sh
+outputPathzsch=./my_scripts/zsch_backup.sh
 
-sed -n '/<Leandro Parisi Scripts/,/Leandro Parisi Scripts>/p' $bashFilePath > $outputPath
+
+sed -n '/<Leandro Parisi Scripts/,/Leandro Parisi Scripts>/p' $bashFilePath > $outputPathCustomScripts
+
+cp $bashFilePath $outputPathzsch
 
 
 vs_code_config_dir="$HOME/.config/Code/User";
